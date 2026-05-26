910dd45168fee2b314d4982f7f006db67740f1fd
Ismailalhazzaa <96993390+Ismailalhazzaa@users.noreply.github.com>
2026-05-26 23:24:52 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,3 @@
-gunicorn --bind=0.0.0.0:8000 --timeout=180 --workers=1 app:app
+#!/bin/bash
+apt-get update -qq && apt-get install -y ffmpeg
+gunicorn --bind=0.0.0.0:8000 --timeout=1800 --workers=1 app:app
